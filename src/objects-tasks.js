a89ed1a0366f5b0a36b2505bed8d33dadd949487
Violetta Vinakova <vinakova.vita@gmail.com>
2025-12-16 16:07:01 +0300

    feat: implement  compareObjects()

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -81,8 +81,15 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  const outliers = keys1.filter((key) => obj1[key] !== obj2[key]);
+
+  return outliers.length === 0;
 }
 
 /**
